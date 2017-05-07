ecf271951116b53bb04eabf377799155e63fbeb0
CHIEH-WEN YANG <chuck@ninethreads.com>
2017-05-07 22:27:52 +0800

    Add examples for the string.

---
+++ String.swift
@@ -0,0 +1,20 @@
+print("Hello World")
+
+let helloString = "Hello"
+let worldString = "World"
+let combinedString = helloString + worldString
+print(helloString)
+print(worldString)
+print(combinedString)
+
+let myNumber = 10
+let theTruth = "My lucky number is \(myNumber)"
+print(myNumber)
+print(theTruth)
+
+let myString = "1"
+Int(myString)
+
+let pi = "3.141596"
+Floot(pi)
+Double(pi)
