3636233986c0f76e86310caea0452a4ec775f240
楊傑文 CHUCK YANG <chuck@ninethreads.com>
2017-05-11 11:14:20 +0800

    Add Color Finder

---
+++ ColorFinder/ColorFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorFinder
+//
+//  Created by Chieh-Wen Yang on 11/05/2017.
+//  Copyright © 2017 NineThreads Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
