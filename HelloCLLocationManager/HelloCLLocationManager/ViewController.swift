2a2701b5be39a30151fd1ae0b797c967b47c124b
楊傑文 CHUCK YANG <chuck@ninethreads.com>
2017-05-18 16:49:35 +0800

    Add HelloCLLocationManager

--- HelloCLLocationManager/HelloCLLocationManager/ViewController.swift
+++ HelloCLLocationManager/HelloCLLocationManager/ViewController.swift
@@ -10,27 +10,43 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var map: MKMapView!
     var locationManager:CLLocationManager?
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager = CLLocationManager()
+        locationManager?.delegate = self
         locationManager?.requestWhenInUseAuthorization()
+        
+        locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager?.activityType = .automotiveNavigation
+        locationManager?.startUpdatingLocation()
+        
         if let coordinate = locationManager?.location?.coordinate {
-            let xScale:CLLocationDegrees = 0.01
-            let yScale:CLLocationDegrees = 0.01
+            let xScale: CLLocationDegrees = 0.01
+            let yScale: CLLocationDegrees = 0.01
             let span: MKCoordinateSpan = MKCoordinateSpanMake(yScale, xScale)
-            
             let region = MKCoordinateRegionMake(coordinate, span)
             map.setRegion(region, animated: true)
         }
         
+        map.userTrackingMode = .followWithHeading
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let coordinate = locations[0].coordinate
+        print("---------------------")
+        print(coordinate.latitude)
+        print(coordinate.longitude)
     }
 
+    override func viewDidDisappear(_ animated: Bool) {
+        locationManager?.stopUpdatingLocation()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
